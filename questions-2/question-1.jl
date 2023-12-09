b311493e68cbc5ae60e7cbadb85d396c3bf49763
Léo Vasconcelos <leohernandesvasc@gmail.com>
2023-12-09 18:04:30 -0300

    add q1 and q2 of list 2

---
+++ questions-2/question-1.jl
@@ -0,0 +1,51 @@
+using JuMP
+using GLPK
+
+model = Model(GLPK.Optimizer)
+
+# Variables:
+# A 3x3 matrix with integer numbers between 1 and 9
+@variable(model, 1 <= x[1:3, 1:3] <= 9, Int)
+
+# Constraints:
+# Each row, column, and diagonal sum to 15
+for i in 1:3
+    # Row sum
+    @constraint(model, sum(x[i, :]) == 15)  
+    # Column sum
+    @constraint(model, sum(x[:, i]) == 15)  
+end
+
+# Main diagonal
+@constraint(model, sum(x[i, i] for i in 1:3) == 15)  
+# Secondary diagonal
+@constraint(model, sum(x[i, 4 - i] for i in 1:3) == 15)
+
+# Ensures that all variables are different
+for i in 1:3
+    for j in 1:3
+        for l in j : 3
+            for k in i + 1 : 3    
+                @constraint(model, 3 <= x[i, j] + x[k, l] <= 17)
+            end
+        end
+    end
+end
+
+# Objective: Minimize a dummy variable (not really needed for this problem)
+@objective(model, Min, sum(x))
+
+# Soluciona o modelo
+optimize!(model)
+
+@show objective_value(model)
+@show value.(x)
+
+# Results:
+# The magic matrix is:
+#
+# ---------
+# | 6 1 8 |
+# | 7 5 3 |
+# | 2 9 4 |
+# ---------
---
+++ questions-2/question-2.jl
@@ -0,0 +1,52 @@
+using JuMP
+using GLPK
+
+model = Model(GLPK.Optimizer)
+
+# Variables:
+# Binary variable for each cost to ignoew:
+# 1 => ignore
+# 0 => consider
+@variable(model, ignore_gold, Bin)
+@variable(model, ignore_time, Bin)
+@variable(model, ignore_mana, Bin)
+# How many times an attack was used
+@variable(model, mammon >= 0, Int)
+@variable(model, arrow >= 0, Int)
+@variable(model, flame >= 0, Int)
+
+BIG_VALUE = 10000
+
+# Constraints:
+# May ignore at most 1 cost 
+@constraint(model, ignore_gold + ignore_time + ignore_mana <= 1)
+# Use at most 2000 gold
+@constraint(model, mammon * 250 + arrow * 50 <= 2000 + ignore_gold*BIG_VALUE)
+# Spend at most 1 minute
+@constraint(model, mammon * 7 + arrow * 10 + flame * 15 <= 60 + ignore_time*BIG_VALUE)
+# Use at most 1000 magic points
+@constraint(model, mammon * 100 + arrow * 150 + flame * 200 <= 1000 + ignore_mana*BIG_VALUE)
+
+# Objective: Do the maximum damage
+@objective(model, Max, mammon * 2000 + arrow * 1750 + flame * 1000)
+
+# Soluciona o modelo
+optimize!(model)
+
+@show objective_value(model)
+@show value(ignore_gold)
+@show value(ignore_time)
+@show value(ignore_mana)
+@show value(mammon)
+@show value(arrow)
+@show value(flame)
+
+# Results
+# The ignored cost is 'time'
+#
+# Times the attacks were used:
+# - Mammon: 7
+# - Arrow: 2
+# - Flame: 0
+#
+# This equals a total of 17_500 points of damage
